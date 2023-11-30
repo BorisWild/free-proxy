826c8db5f3b32d56c2353811037ed9af92d0989c
boris <boriswild@gmail.com>
2023-12-01 01:46:39 +0300

    composer upd

---
+++ laravel/database/migrations/2023_11_30_211251_create_product_attachments_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('product_attachments', function (Blueprint $table) {
+            $table->uuid('id');
+            $table->primary('id');
+            $table->string('title')->nullable();
+            $table->text('description')->nullable();
+            $table->string('path')->nullable();
+
+            $table->uuid('product_id');
+            $table->foreign('product_id')
+                ->references('id')
+                ->on('products')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('product_attachments');
+    }
+};
