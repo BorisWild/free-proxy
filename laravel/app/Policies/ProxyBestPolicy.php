0d4e051829d63ab48418db921ed32f89bed1b5b0
Boris Wild <boriswild@gmail.com>
2024-01-04 00:18:37 +0300

    add proxy best policy

--- laravel/app/Policies/ProxyBestPolicy.php
+++ laravel/app/Policies/ProxyBestPolicy.php
@@ -2,11 +2,11 @@
 
 namespace App\Policies;
 
-use App\Models\Product;
+use App\Models\ProxyBestModel;
 use App\Models\User;
 
 
-class ProductPolicy
+class ProxyBestPolicy
 {
     /**
      * Determine whether the user can view any models.
@@ -19,9 +19,9 @@ class ProductPolicy
     /**
      * Determine whether the user can view the model.
      */
-    public function view(User $user, Product $product): bool
+    public function view(User $user, ProxyBestModel $product): bool
     {
-        return $user->id===$product->user_id;
+        return true;
     }
 
     /**
@@ -35,31 +35,31 @@ class ProductPolicy
     /**
      * Determine whether the user can update the model.
      */
-    public function update(User $user, Product $product): bool
+    public function update(User $user, ProxyBestModel $product): bool
     {
-        return $user->id===$product->user_id;
+        return $user->email==='boriswild@gmail.com';
     }
 
     /**
      * Determine whether the user can delete the model.
      */
-    public function delete(User $user, Product $product): bool
+    public function delete(User $user, ProxyBestModel $product): bool
     {
-        return $user->id===$product->user_id;
+        return $user->email==='boriswild@gmail.com';
     }
 
     /**
      * Determine whether the user can restore the model.
      */
-    public function restore(User $user, Product $product): bool
+    public function restore(User $user, ProxyBestModel $product): bool
     {
-        return $user->id===$product->user_id;
+        return true;
     }
 
     /**
      * Determine whether the user can permanently delete the model.
      */
-    public function forceDelete(User $user, Product $product): bool
+    public function forceDelete(User $user, ProxyBestModel $product): bool
     {
         return false;
     }
